b971a5d7268033cc3d9e49d5543f878dfafe4cc1
GFROCRAI <gwenda.frocrain@gmail.com>
2016-08-03 16:45:58 +0200

    make iteration for cards + add modal to cards + chang some css for the cards

---
+++ source/javascripts/navbar_scroll.js
@@ -0,0 +1,16 @@
+// Navbar transition on scroll
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 300) {
+      $(".navbar-wagon").css({
+        "margin-top": "-70px"
+      });
+    }
+    else {
+      $(".navbar-wagon").css({
+        "margin-top": "0",
+      });
+    }
+  });
+});
